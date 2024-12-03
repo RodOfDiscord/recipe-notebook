01395e32a24a82effb29e3579864da759863b343
Ivan <ipz222_pis@student.ztu.edu.ua>
2024-12-04 00:05:47 +0200

    feat: add recipe page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import style from "./style.module.css";
 import RecipeList from "../../modules/RecipeList/RecipeList";
+import Button from "../../ui/Button/Button";
 import Typography from "../../ui/Typography/Typography";
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <>
-      <Typography variant="h1">Recipe NoteBook📝🍲</Typography>
+      <div className={style.container}>
+        <Typography variant="h1">Recipe NoteBook📝🍲</Typography>
+        <Button onClick={() => navigate("/add")}>Add Recipe</Button>
+      </div>
+
       <RecipeList></RecipeList>
     </>
   );
--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,5 +1,29 @@
+import Typography from "../../ui/Typography/Typography";
+import style from "./style.module.css";
+import { useParams, useNavigate } from "react-router-dom";
+import { RecipesAtom } from "../../atoms/recipesAtom";
+import { useAtom } from "jotai";
+
 function RecipePage() {
-  return;
+  const [recipes] = useAtom(RecipesAtom);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const recipe = recipes.find((recipe) => recipe.id === parseInt(id));
+  if (!recipe) navigate("/");
+  return (
+    <>
+      <Typography variant="h1">{recipe.title}</Typography>
+      <Typography variant="p">{recipe.description}</Typography>
+      <Typography variant="h2">Steps:</Typography>
+      <ol className={style["steps-list"]}>
+        {recipe.steps.map((step, index) => (
+          <li key={index}>
+            <Typography>{step.content}</Typography>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
 export default RecipePage;
